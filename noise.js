1f2505df9f8a04e03e8db25f5eed5c104a01d58f
Katricia Smith <smithkatricia@gmail.com>
2018-11-19 16:46:40 -0800

    Add logic for jquery on click sounds

--- noise.js
+++ noise.js
@@ -1,3 +1,19 @@
 $(document).ready( function() {
   // your code here
+  // const keys = []
+  // const note = $(this.html)
+  // $('.note c').click
+
+  // $('body').click(function(event))
+   // $('.instrument').on('click', 'button', function(event) {
+      $('.instrument').on('click', 'button',
+      function() {
+        const note = $(this).html();
+        console.log(`played ${note}`);
+        $(`#${note}Audio`)[0].play();
+   }
+ );
+
+ $
+
 });
